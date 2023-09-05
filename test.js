9e2db38d0b3df08dfc9a590e7a3e9e099e900a61
Rana Shahid <ranashahidikram7@gmail.com>
2023-09-05 14:52:19 +0500

    Use of Object Type / Declaration Object Type

---
+++ test.js
@@ -0,0 +1,52 @@
+"use strict";
+let students = [
+    {
+        Name: "Abdullah",
+        F_Name: "Shahid",
+        Roll_No: 1,
+    },
+    {
+        Name: "Asad",
+        F_Name: "Ikram",
+        Roll_No: 2,
+    },
+    {
+        Name: "Ahsan",
+        F_Name: "Amir",
+        Roll_No: 3,
+    },
+];
+for (let stu = 0; stu < students.length; stu++) {
+    console.log(students[stu]);
+}
+let Doctor = [
+    {
+        Dr_Name: "Dr.Khawar Saeed",
+        Ward: "Children Ward",
+        Shift: "First",
+        Shift_Time: "8:00 am to 4:00pm"
+    },
+    {
+        Dr_Name: "Dr.Zubair",
+        Ward: "General Ward",
+        Shift: "Second",
+        Shift_Time: "4:00 pm to 12:00pm"
+    },
+    {
+        Dr_Name: "Dr.Kamran",
+        Ward: "Orthopedic Ward",
+        Shift: "Third",
+        Shift_Time: "12:00 am to 8:00am"
+    },
+];
+let doc = 0;
+while (doc < Doctor.length) {
+    console.log(Doctor[doc]);
+    doc++;
+}
+for (let Doctors of Doctor) {
+    console.log("Doctor Name is = ", Doctors.Dr_Name);
+    console.log("Ward = ", Doctors.Ward);
+    console.log("Duty Shift= ", Doctors.Shift);
+    console.log("Shift_Time = ", Doctors.Shift_Time);
+}
